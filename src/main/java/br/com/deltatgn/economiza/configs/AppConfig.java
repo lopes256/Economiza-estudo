ffddb8bca22a1b184b77682273d6215d613f0468
Jammerson Lopes <lopes256@gmail.com>
2024-05-29 10:31:00 -0300

    Ajuste AppConfig

--- src/main/java/br/com/deltatgn/economiza/configs/AppConfig.java
+++ src/main/java/br/com/deltatgn/economiza/configs/AppConfig.java
@@ -1,5 +1,6 @@
 package br.com.deltatgn.economiza.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +12,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
     
+    private final String so = String.valueOf(System.getProperty("os.name"));
+    private final String version = String.valueOf(System.getProperty("os.version"));
+
+    @Value("${APPLICATION_AMBIENTE}")
+    private String ambiente;
     
-    
+    @Value("${application.description}")
+    private String projeto;
+
     @Bean
-    public CommandLineRunner executar() {
+    public CommandLineRunner execulteAppConfig() {
         return args -> {
-            System.out.println("Iniciando!!!");
+            System.out.println("=============================================================");
+            System.out.println("Sistema Operacional: " + so);
+            System.out.println("Versão: " + version);
+            System.out.println("Projeto: " + projeto);
+            System.out.println("Ambiente: " + ambiente);
+            System.out.println("=============================================================");
         };
     }
+
 }
